2961ee6ec6e38e59a7e2fe3d9e79020cf19b7095
nickmcmahonHOD <nick.mcmahon@digital.homeoffice.gov.uk>
2018-03-20 01:06:14 +0000

    force 1 message per poll max

--- src/main/scala/uk/gov/homeoffice/aws/sqs/SQS.scala
+++ src/main/scala/uk/gov/homeoffice/aws/sqs/SQS.scala
@@ -1,8 +1,7 @@
 package uk.gov.homeoffice.aws.sqs
 
 import scala.collection.JavaConversions._
-import com.amazonaws.services.sqs.model.{QueueDoesNotExistException, SendMessageResult}
-
+import com.amazonaws.services.sqs.model.{QueueDoesNotExistException, SendMessageResult, ReceiveMessageRequest}
 class SQS(val queue: Queue)(implicit val sqsClient: SQSClient) extends QueueCreation {
   create(queue)
 
@@ -17,7 +16,9 @@ class SQS(val queue: Queue)(implicit val sqsClient: SQSClient) extends QueueCrea
   private def publish(message: String, queueName: String): SendMessageResult = sqsClient.sendMessage(queueUrl(queueName), message)
 
   private def receive(queueName: String): Seq[Message] = try {
-    sqsClient.receiveMessage(queueUrl(queueName)).getMessages.map(Message)
+    var rec = new ReceiveMessageRequest(queueUrl(queueName))
+    rec.setMaxNumberOfMessages(1)
+    sqsClient.receiveMessage(rec).getMessages.map(Message)
   } catch {
     case t: QueueDoesNotExistException =>
       t.printStackTrace()
